53069346bd5637d4cee22d5672e3fd45f69b2055
David Elentok <3david@gmail.com>
2014-07-23 23:57:42 +0300

    Extract version into variable set during build
    * The build script sets the "main.version" variable

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 	"github.com/elentok/gever/commands"
 )
 
+// this value is replaced when building:
+//
+//   go build -ldflags "-X main.version #{version}" main.go
+var version string = "DEV"
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "gever"
 	app.Usage = "semver-based versioning tool"
+	app.Version = version
 	app.Commands = []cli.Command{
 		commands.Show,
 		commands.Create,
